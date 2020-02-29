5ca1adaad18e34c7d8f7d2d6c64e613219be8221
Chris Lucas <chris@chrisjlucas.com>
2020-02-29 10:22:27 -0500

    Remove comments in Ralph.Application

--- lib/ralph/application.ex
+++ lib/ralph/application.ex
@@ -1,21 +1,13 @@
 defmodule Ralph.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
     children = [
-      # Ralph.Console
       Ralph.Bot
     ]
 
-    # IO.stream(:stdin, :line)
-    # |> Enum.each(&IO.inspect/1)
-
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Ralph.Supervisor]
     Supervisor.start_link(children, opts)
   end
